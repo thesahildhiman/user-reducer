36581a84ff7889814a61f074a56e28bd80270c2f
thesahildhiman <thesahildhiman@gmail.com>
2023-06-28 08:18:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+function App() {
+  const initialValue = 0;
+  const [state, dispatch] = useReducer(reducer, initialValue);
+  return (
+    <div className="App">
+      <h2>{state}</h2>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
+    </div>
+  );
+}
+
+export default App;
